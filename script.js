819b206ccf8e215e166233f5f33ab55408bbb043
SimKitkat <simone.cornelius14@gmail.com>
2025-04-15 18:56:15 +0200

    mobile responsive site
    initial website setup

---
+++ script.js
@@ -0,0 +1,7 @@
+// Optional: simple form feedback
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thanks for reaching out! I’ll get back to you soon 📸✨");
+  });
